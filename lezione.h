71f2f1e3c31e485ac03a3cbd1adf874278a97649
anxrdx <andrea.zottoli09@gmail.com>
2025-04-27 10:53:48 +0200

    creato file lezione.h
    file già commentato che crea la struct lezione

---
+++ lezione.h
@@ -0,0 +1,9 @@
+typedef struct //creazione della struct
+ {
+
+	int max_partecipanti; //limite fisso di partecipanti per ogni lezione
+	char giorno[20]; //giorno della settimana
+	char orario[20]; //tipo: 10-12
+	char data[11]; // giorno/mese/anno
+
+} lezione; //nome della struct
