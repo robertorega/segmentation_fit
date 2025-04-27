b8523f755f1fc7eb1b211f247bc567f34229b176
anxrdx <andrea.zottoli09@gmail.com>
2025-04-27 11:10:19 +0200

    creazione file abbonati.h
    ho creato i tipi abbonati e abbonamenti

---
+++ abbonati.h
@@ -0,0 +1,14 @@
+#define limite_caratteri 30
+
+typedef struct{
+	char tipo[limite_caratteri];
+	int durata;
+} abbonamento;
+
+typedef struct{
+	char nome[limite_caratteri];
+	char cognome[limite_caratteri];
+	int annodinascita;
+	int id;
+	abbonamento tipo_abbonamento;
+} abbonato;
