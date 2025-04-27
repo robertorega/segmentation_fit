d04524776856ffb05efc5c272ad98767f5d210c8
anxrdx <andrea.zottoli09@gmail.com>
2025-04-27 10:43:38 +0200

    file contattaci.c
    ho creato l'interfaccia per i contatti all'interno di questo file

---
+++ contattaci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "contattaci.h"
+
+void contattaci() 
+{
+
+	int scelta = 0;
+
+	printf("\nHai bisogno di ulteriori info?\n");
+	printf("Ci trovi in Via degli Atleti, n51\n");
+	printf("Altrimenti chiamaci ai seguenti numeri:\n");
+	printf("Andrea Zottoli - 0512122412\n");
+	printf("Roberto Rega - 0512121583\n");
+	printf("I nostri orari: LUN-VEN 07:00 - 22:00, SAB 08:30 - 17:00, DOM CHIUSI\n\n");
+
+	printf("Possiamo fare altro per te? Premi INVIO\n");
+    
+	getchar(); getchar();
+}
