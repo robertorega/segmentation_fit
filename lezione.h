44b5540a4c06d7b6687eb521b9ae41d1c473fbd4
robertorega <r.rega24@studenti.unisa.it>
2025-05-06 14:14:07 +0200

    Aggiornato lezione.h
    Inserita la stack iscritti nella struct lezione

--- lezione.h
+++ lezione.h
@@ -1,9 +1,6 @@
-typedef struct //creazione della struct
- {
-
-	int max_partecipanti; //limite fisso di partecipanti per ogni lezione
+typedef struct lezione{
+	stack iscritti; 
 	char giorno[20]; //giorno della settimana
 	char orario[20]; //tipo: 10-12
 	char data[11]; // giorno/mese/anno
-
-} lezione; //nome della struct
+} lezione; 
