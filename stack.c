2878253cc65d3b863173b34237cb8466bb5a8d9b
robertorega <r.rega24@studenti.unisa.it>
2025-05-06 14:25:19 +0200

    Aggiornato stack.c
    "Pulizia" del file stack.c

--- stack.c
+++ stack.c
@@ -9,47 +9,42 @@ struct c_stack {
 	int top;
 };
 
-stack newStack(void)
-{
+stack newStack(void){
 	stack iscritti;
 	iscritti = malloc(sizeof(struct c_stack));
-	if (iscritti == NULL) return NULL;
+	if (iscritti == NULL)
+		return NULL;
 	iscritti->top = 0;
 	return iscritti;
 }
 
-int emptyStack(stack iscritti)
-{
+int emptyStack(stack iscritti){
 	return iscritti->top == 0;
 }
 
-int push(partecipante val, stack iscritti)
-{
+int push(partecipante val, stack iscritti){
 	if (iscritti->top == MAXSTACK)
-    	return 0;
+    		return 0;
 	strcpy(iscritti->vet[iscritti->top], val);
 	(iscritti->top)++;
 	return 1;
 }
 
-int pop(stack iscritti)
-{
+int pop(stack iscritti){
 	if (iscritti->top == 0)
-    	return 0;
+    		return 0;
 	(iscritti->top)--;
 	return 1;
 }
 
-char* top(stack iscritti)
-{
+char* top(stack iscritti){
 	if (iscritti->top > 0)
-    	return iscritti->vet[iscritti->top - 1];
+    		return iscritti->vet[iscritti->top - 1];
 	else
-    	return NULLPARTECIPANTE;
+    		return NULLPARTECIPANTE;
 }
 
-int stackSize(stack iscritti)
-{
+int stackSize(stack iscritti){
 	if (iscritti == NULL)
     		return -1;  
 	return iscritti->top; // numero partecipanti
