5a3ffff8aa580233e9dbec905fbf6aea743c094c
robertorega <r.rega24@studenti.unisa.it>
2025-05-06 14:15:57 +0200

    Aggiornato contattaci.c
    "Pulizia" file contattaci.c

--- contattaci.c
+++ contattaci.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "contattaci.h"
 
-void contattaci() 
-{
+void contattaci() {
 	printf("\nHai bisogno di ulteriori info?\n");
 	printf("Ci trovi in Via degli Atleti, n51\n");
 	printf("Altrimenti chiamaci ai seguenti numeri:\n");
@@ -11,5 +10,5 @@ void contattaci()
 	printf("I nostri orari: LUN-VEN 07:00 - 22:00, SAB 08:30 - 17:00, DOM CHIUSI\n\n");
 
 	printf("Possiamo fare altro per te? Premi INVIO\n");
-    getchar(); getchar();
+    	getchar(); getchar();
 }
