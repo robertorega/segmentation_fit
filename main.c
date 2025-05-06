303b248f418290e9b14318187daedbd9f22af5df
robertorega <r.rega24@studenti.unisa.it>
2025-05-06 14:12:53 +0200

    Aggiornato main.c
    "Pulizia" del file main.c

--- main.c
+++ main.c
@@ -6,52 +6,50 @@
 
 int main(){
 	int scelta=0;
-
 	queue calendario = newQueue();
+	
     	printf("Segmentation Fit\n");
     	printf("Benvenuti nella palestra numero uno per gli informatici\n\n");
 
-	while (1) {
-
-    	printf("Selezionare un’opzione:\n");
-    	printf("1 - abbonati\n");
-    	printf("2 - lezioni\n");
-   	 	printf("3 - lezioni più frequentate\n");
-   	 	printf("4 - login abbonati\n");
-   	 	printf("5 - disdici\n");
- 	   	printf("6 - contattaci\n");
-    	printf("7 - esci\n");
+	while (1){
+    		printf("Selezionare un’opzione:\n");
+    		printf("1 - abbonati\n");
+    		printf("2 - lezioni\n");
+   		printf("3 - lezioni più frequentate\n");
+   		printf("4 - login abbonati\n");
+   		printf("5 - disdici\n");
+		printf("6 - contattaci\n");
+    		printf("7 - esci\n");
 
 		printf("La tua scelta: ");
     		scanf("%d", &scelta);
 
-    	switch(scelta)
-		{
-        	case 1:
-            	printf("Funzione ancora da implementare.\n");
-            	break;
-        	case 2:
-            	printf("Funzione ancora da implementare.\n");
-            	break;
-        	case 3:
-            	printf("Funzione ancora da implementare.\n");
-            	break;
- 		    case 4:
-            	printf("Funzione ancora da implementare.\n");
-            	break;
-    		case 5:
+    		switch(scelta){
+        		case 1:
+            		printf("Funzione ancora da implementare.\n");
+            		break;
+        		case 2:
+            		printf("Funzione ancora da implementare.\n");
+            		break;
+        		case 3:
+            		printf("Funzione ancora da implementare.\n");
+            		break;
+ 		    	case 4:
+            		printf("Funzione ancora da implementare.\n");
+            		break;
+    			case 5:
       			printf("Funzione ancora da implementare.\n");
      			break;
-    		case 6:
+    			case 6:
            		contattaci();
           		break;
-        	case 7:
-            	printf("Arrivederci!\n");
-            	return 0;  // esce dal programma
-        	default:
-            	printf("Valore non valido\n\n");
-            	break;
-    	}
+        		case 7:
+            		printf("Arrivederci!\n");
+            		return 0;  // esce dal programma
+        		default:
+            		printf("Valore non valido\n\n");
+            		break;
+    		}
 	}
 	return 0;
 }
