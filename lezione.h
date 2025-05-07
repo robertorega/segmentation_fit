be7ecf03a7530e8d4ed129ac8b02397da81d63ce
robertorega <r.rega24@studenti.unisa.it>
2025-05-07 14:59:58 +0200

    Aggiornato lezione.h
    Aggiunti commenti

--- lezione.h
+++ lezione.h
@@ -2,7 +2,7 @@
 
 typedef struct lezione{
 	stack iscritti; 
-	char giorno[20]; //giorno della settimana
-	char orario[20]; //tipo: 10-12
-	char data[11]; // giorno/mese/anno
+	char giorno[20]; //Giorno della settimana in cui si tiene la lezione
+	char orario[20]; //Fascia oraria della lezione
+	char data[11]; //gg/mm/aa
 } lezione; 
