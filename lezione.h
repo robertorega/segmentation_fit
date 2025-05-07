0ce753039c337deab4e5318f6f3ed938c7e2dfb7
robertorega <r.rega24@studenti.unisa.it>
2025-05-07 15:10:59 +0200

    Aggiornato lezione.h
    Aggiornate scelte stilistiche

--- lezione.h
+++ lezione.h
@@ -1,8 +1,8 @@
 #include "stack.h"
 
-typedef struct lezione{
+typedef struct lezione {
 	stack iscritti; 
 	char giorno[20]; //Giorno della settimana in cui si tiene la lezione
 	char orario[20]; //Fascia oraria della lezione
 	char data[11]; //gg/mm/aa
-} lezione; 
+} lezione;
