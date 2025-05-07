d98501b1b314f6f83bcf104a61dad3559e6e0552
robertorega <r.rega24@studenti.unisa.it>
2025-05-07 15:10:24 +0200

    Aggiornato main.c
    Aggiornate scelte stilistiche

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "contattaci.h"
 #include "queue.h"
 
-int main(){
+int main() {
 	int scelta=0; // Variabile che memorizza la scelta dell'utente
 	queue calendario = newQueue(); // Crea una nuova coda per il calendario delle lezioni
 
@@ -13,7 +13,7 @@ int main(){
     	printf("Benvenuti nella palestra numero uno per gli informatici\n\n");
 
 	// Ciclo infinito per il menu principale
-	while (1){
+	while (1) {
 		// Stampa le opzioni del menu
     		printf("Selezionare un’opzione:\n");
     		printf("1 - abbonati\n");
@@ -27,7 +27,7 @@ int main(){
 		printf("La tua scelta: "); // Chiede all'utente di inserire la propria scelta
     		scanf("%d", &scelta); // Legge l'input dell'utente
 
-    		switch(scelta){
+    		switch(scelta) {
         		case 1:
             		printf("Funzione ancora da implementare.\n");
             		break;
