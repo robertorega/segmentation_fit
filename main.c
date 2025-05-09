488e161b769b9f5eadd511d09c04816a3fd4a523
robertorega <r.rega24@studenti.unisa.it>
2025-05-09 15:42:35 +0200

    "Pulizia" main.c
    Sistemato ordine file

--- main.c
+++ main.c
@@ -4,52 +4,51 @@
 #include "contattaci.h"
 #include "queue.h"
 
-int main(){
-	int scelta=0;
-	queue calendario = newQueue();
-	
+int main() {
+	int scelta=0; // Variabile che memorizza la scelta dell'utente
+	queue calendario = newQueue(); // Crea una nuova coda per il calendario delle lezioni
+
+	// Messaggio di benvenuto all'utente
     	printf("Segmentation Fit\n");
     	printf("Benvenuti nella palestra numero uno per gli informatici\n\n");
 
-	while (1){
-    		printf("Selezionare un’opzione:\n");
-    		printf("1 - abbonati\n");
-    		printf("2 - lezioni\n");
+	// Ciclo infinito per il menu principale
+	while (1) {
+		// Stampa le opzioni del menu
+    	printf("Selezionare un’opzione:\n");
+    	printf("1 - Area abbonati\n");
+    	printf("2 - lezioni\n");
    		printf("3 - lezioni più frequentate\n");
-   		printf("4 - login abbonati\n");
-   		printf("5 - disdici\n");
-		printf("6 - contattaci\n");
-    		printf("7 - esci\n");
+   		printf("4 - disdici\n");
+		printf("5 - contattaci\n");
+    	printf("6 - esci\n");
 
-		printf("La tua scelta: ");
-    		scanf("%d", &scelta);
+		printf("La tua scelta: "); // Chiede all'utente di inserire la propria scelta
+    	scanf("%d", &scelta); // Legge l'input dell'utente
 
-    		switch(scelta){
-        		case 1:
-            		printf("Funzione ancora da implementare.\n");
-            		break;
-        		case 2:
-            		printf("Funzione ancora da implementare.\n");
-            		break;
-        		case 3:
-            		printf("Funzione ancora da implementare.\n");
-            		break;
- 		    	case 4:
-            		printf("Funzione ancora da implementare.\n");
-            		break;
-    			case 5:
-      			printf("Funzione ancora da implementare.\n");
-     			break;
-    			case 6:
-           		contattaci();
-          		break;
-        		case 7:
-            		printf("Arrivederci!\n");
-            		return 0;  // esce dal programma
-        		default:
-            		printf("Valore non valido\n\n");
-            		break;
-    		}
+    	switch(scelta) {
+        	case 1:
+        	printf("Funzione ancora da implementare.\n");
+        	break;
+        	case 2:
+            printf("Funzione ancora da implementare.\n");
+            break;
+        	case 3:
+            printf("Funzione ancora da implementare.\n");
+            break;
+ 		    case 4:
+            printf("Funzione ancora da implementare.\n");
+            break;
+    		case 5:
+           	contattaci();
+          	break;
+        	case 6:
+            printf("Arrivederci!\n");
+            return 0;  // esce dal programma
+        	default:
+            printf("Valore non valido\n\n"); // Messaggio di errore
+            break;
+    	}
 	}
 	return 0;
 }
