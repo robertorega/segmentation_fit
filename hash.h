3beb2ed9a4c6b8fbb8f22a68229206bb7999c068
anxrdx <andrea.zottoli09@gmail.com>
2025-05-13 10:42:48 +0200

    creazione hash.h
    funzioni per la gestione della tabella hash

---
+++ hash.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "abbonati.h"
+#include "partecipante.h"
+
+typedef struct hash 
+{
+
+	int dimensione;
+	abbonato **tabella; 
+
+} *tabella_hash;
+
+// Funzioni per la gestione della tabella hash
+tabella_hash nuova_hash(int dimensione);
+int calcola_indice(char *chiave, int dimensione);
+tabella_hash inserisci_hash(abbonato item, tabella_hash tabella);
+abbonato *cerca_hash(char *chiave, tabella_hash tabella);
+tabella_hash elimina_hash(char *chiave, tabella_hash tabella);
