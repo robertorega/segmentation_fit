bc256a29b074f194016355ec77fe217f694cd0c6
robertorega <r.rega24@studenti.unisa.it>
2025-05-23 11:57:42 +0200

    Tolto \n in più coda.c
    Tolto un \n di troppo in report_mensile

--- coda.c
+++ coda.c
@@ -957,7 +957,7 @@ void pulisci_lezioni_passate(coda calendario, const char *nome_file)
 void report_mensile()
 {
 	printf("--- Report Mensile ---\n");
-	printf("Visualizza le lezioni passate, ordinate per numero di partecipanti.\n\n");
+	printf("Visualizza le lezioni passate, ordinate per numero di partecipanti.\n");
     FILE *file_storico = fopen("storico.txt", "r");
     if (!file_storico)
     {
