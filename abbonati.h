f4b865479cf50940c750c4a6c1992c7f58996e86
robertorega <r.rega24@studenti.unisa.it>
2025-05-26 15:27:59 +0200

    Aggiunti commenti in abbonati.h
    Aggiunti commenti.

--- abbonati.h
+++ abbonati.h
@@ -1,15 +1,16 @@
 #ifndef ABBONATI_H
 #define ABBONATI_H
 
-#define MAX_CARATTERI 50
+#define MAX_CARATTERI 50 // Dimensione massima per i campi stringa
 
+// Struttura dell'abbonato
 typedef struct abbonato 
 {
-    char *chiave;
-    char nomeutente[MAX_CARATTERI];
-    char password[MAX_CARATTERI];
-    int lezioni_rimanenti;
-    struct abbonato *prossimo;
+    char *chiave; // Stringa univoca per identificare l'abbonato
+    char nomeutente[MAX_CARATTERI]; // Nome utente
+    char password[MAX_CARATTERI]; // Password
+    int lezioni_rimanenti; // Numero di lezioni rimanenti
+    struct abbonato *prossimo; // Puntatore al prossimo abbonato
 } abbonato;
 
 #endif
