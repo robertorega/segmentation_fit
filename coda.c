9b36e7f9876e1f172c65ca100b3d411289b65937
robertorega <r.rega24@studenti.unisa.it>
2025-05-26 18:17:37 +0200

    Errata corrige commenti in coda.c
    Corretto "precondizione" in "precondizioni"

--- coda.c
+++ coda.c
@@ -35,7 +35,7 @@ struct c_coda
 * calendario: la coda dove verranno inserite le lezioni lette dal file.
 * nome_file: nome del file da cui leggere le lezioni e gli iscritti.
 *
-* Pre-condizione:
+* Pre-condizioni:
 * - 'calendario' deve essere una coda inizializzata.
 * - 'nome_file' deve essere un puntatore valido a una stringa non nulla.
 *
