8b32de9a1b4b92fa3fdf4089d48c5df3546fa374
robertorega <r.rega24@studenti.unisa.it>
2025-05-26 15:26:29 +0200

    Aggiunti commenti in partecipante.h
    Aggiunti commenti.

--- partecipante.h
+++ partecipante.h
@@ -1,8 +1,9 @@
 #ifndef PARTECIPANTE_H
 #define PARTECIPANTE_H
 
-#define NESSUN_PARTECIPANTE ""
+#define NESSUN_PARTECIPANTE "" // Assenza del partecipante
 
+// Tipo per rappresentare un partecipante ad una lezione
 typedef char partecipante[50];
 
 #endif
