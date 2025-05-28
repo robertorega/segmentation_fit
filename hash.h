8430160bde8a761380c0f8eae69f250fa955ef28
robertorega <r.rega24@studenti.unisa.it>
2025-05-28 17:36:19 +0200

    Rimosse funzioni ausiliari da hash.h
    Rimosse funzioni carica/salva abbonati, spostate in utile_hash.

--- hash.h
+++ hash.h
@@ -76,40 +76,4 @@ tabella_hash inserisci_hash(abbonato item, tabella_hash tabella);
 */
 abbonato *cerca_hash(char *chiave, tabella_hash tabella);
 
-
-/* Funzione: carica_abbonati
-*
-* Carica da file i dati degli abbonati e li inserisce in una nuova tabella hash
-*
-* Parametri:
-* nome_file: nome del file da cui leggere i dati
-*
-* Pre-condizione:
-* nome_file è un puntatore a stringa non nullo
-*
-* Post-condizione:
-* Restituisce una tabella hash inizializzata contenente tutti gli abbonati letti dal file.
-* Se il file non esiste, la tabella restituita è vuota.
-*
-* Side-effect:
-* Lettura da file e allocazione dinamica di memoria per ciascun abbonato caricato
-*/
-tabella_hash carica_abbonati(const char *nome_file);
-
-/* Funzione: salva_abbonati
-*
-* Salva su file i dati degli abbonati presenti nella tabella hash
-*
-* Parametri:
-* h: tabella hash contenente gli abbonati da salvare
-* nome_file: nome del file su cui salvare i dati
-*
-* Pre-condizione:
-* h è una tabella hash valida e nome_file è un puntatore a stringa non nullo
-*
-* Side-effect:
-* Scrittura su file. Se il file non può essere aperto, viene stampato un messaggio di errore
-*/
-void salva_abbonati(tabella_hash h, const char *nome_file);
-
 #endif
